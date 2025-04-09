64566baf72820f6a4336b31b2619f3e2cc02d19b
Phat Nguyen <nguyentanphat1304@gmail.com>
2025-04-09 22:28:54 +0700

    L70

--- excelDemo2.js
+++ excelDemo2.js
@@ -1,23 +1,34 @@
 const ExcelJs = require('exceljs');
 
-async function excelTest() {
-    let output = {row:-1,column:-1};
+
+async function writeExcelTest(searchText,replaceText,change,filePath) {
+    
     const workbook = new ExcelJs.Workbook();
-    await workbook.xlsx.readFile("sample.xlsx")
+    await workbook.xlsx.readFile(filePath)
     const workSheet = workbook.getWorksheet('Sheet1');
+    const output = await readExcel(workSheet,searchText);
+    if(output.row === -1 && output.column === -1) {
+        console.log("Search text not found");
+        return;
+    }
+    const cell = workSheet.getCell(output.row,output.column+change.colChange);
+    cell.value = replaceText;
+    await workbook.xlsx.writeFile(filePath);
+}
+
+async function readExcel(workSheet,searchText){
+    let output = {row:-1,column:-1};
     workSheet.eachRow((row, rowNumber) => {
         row.eachCell((cell, colNumber) => {
-            if(cell.value === "Banana") {
-                //console.log(rowNumber,colNumber)
+            if(cell.value === searchText) {
                 output.row = rowNumber;
                 output.column = colNumber;
             }
         })
     })
-
-    const cell = workSheet.getCell(output.row,output.column);
-    cell.value = "Republic";
-    await workbook.xlsx.writeFile("sample.xlsx");
+    //console.log(output);
+    return output;
 }
 
-excelTest();
+//change price to 350
+writeExcelTest("Mango","350",{rowChange:0,colChange:2},"sample.xlsx");
